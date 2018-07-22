18f8ff93b484f89eedb386ba7b80d234ae137d4f
Sean Knowles <sean.code@outlook.com>
2018-07-22 12:22:06 +0200

    initialised mock logData in logs.component and linked mock data and view

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogsComponent implements OnInit {
 
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
+
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      { id: '1', text: 'Generated Component', date: new Date('01/25/2018 12:54:23') },
+      { id: '2', text: 'Added Bootstrap', date: new Date('02/27/2018 09:33:23') },
+      { id: '3', text: 'Added Logs Component', date: new Date('12/20/2018 08:54:23') }
+    ];
   }
 
 }
