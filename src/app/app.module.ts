cf878da136cf2de4a04974fb7483f0fb2e109884
Sean Knowles <sean.code@outlook.com>
2018-07-22 11:08:41 +0200

    generated components, log-form, logs and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogsComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule
