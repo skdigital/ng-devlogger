5ecd83c39ad11bcae280c11145bff84ada268f2e
Sean Knowles <sean.code@outlook.com>
2018-07-22 12:27:35 +0200

    comments added cleaner formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
+
+// Services
 import { LogService } from './services/log.service';
 
 @NgModule({
