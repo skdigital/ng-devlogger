6494078766e3fc897184bbcdc8955a92a6037c16
Sean Knowles <sean.code@outlook.com>
2018-07-22 12:26:16 +0200

    cleaned up spec files and added log.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogService } from './services/log.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { LogFormComponent } from './components/log-form/log-form.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
